0b839c5eae4673a44c47120e48708d729bc7b0b4
Nicu Barbu <barbu_nicusor6@yahoo.com>
2022-11-15 01:15:51 +0200

    Update tests.py
    Added tests for F1 -> F13

--- Tests/tests.py
+++ Tests/tests.py
@@ -83,62 +83,81 @@ def print_packages_for_destination_and_price():
     op.add_package(all_packages, 2, "01/01/2022", "01/01/2022", "Paris", 700)
     op.add_package(all_packages, 3, "01/01/2022", "01/01/2022", "Bucharest", 200)
     assert op.print_packages_for_destination_and_price(all_packages, "Paris", 600)
+    assert (domain.get_id(all_packages[0]) == 1)
+    assert (domain.get_destination(all_packages[0]) == "Paris")
+    assert (domain.get_start_date(all_packages[0]) == "01/01/2022")
+    assert (domain.get_end_date(all_packages[0]) == "01/01/2022")
+    assert (domain.get_price(all_packages[0]) == 500)
+    assert (domain.get_id(all_packages[1]) == 2)
+    assert (domain.get_destination(all_packages[1]) == "Paris")
+    assert (domain.get_start_date(all_packages[1]) == "01/01/2022")
+    assert (domain.get_end_date(all_packages[1]) == "01/01/2022")
+    assert (domain.get_price(all_packages[1]) == 700)
     
     
-# def print_packages_for_end_date():
-#     all_packages = []
-#     op.add_package(all_packages, "10/02/2022", "14/02/2022", "Bucharest", 500)
-#     op.add_package(all_packages, "07/01/2022", "10/01/2022", "Bologna", 250)
-#     op.add_package(all_packages, "01/01/2022", "03/01/2022", "Paris", 1500)
-#     result = op.print_packages_for_end_date(all_packages, "15/01/2022") == ["Paris\n", "Bologna\n"]
+def print_packages_for_end_date():
+    all_packages = []
+    op.add_package(all_packages, 1, "07/01/2022", "10/01/2022", "Bologna", 250)
+    op.add_package(all_packages, 2, "10/02/2022", "14/02/2022", "Bucharest", 500)
+    op.add_package(all_packages, 3, "01/01/2022", "03/01/2022", "Paris", 1500)
+    assert op.print_packages_for_end_date(all_packages, "10/01/2022")
+    assert (domain.get_id(all_packages[0]) == 1)
+    assert (domain.get_destination(all_packages[0]) == "Bologna")
+    assert (domain.get_start_date(all_packages[0]) == "07/01/2022")
+    assert (domain.get_end_date(all_packages[0]) == "10/01/2022")
+    assert (domain.get_price(all_packages[0]) == 250)
     
     
-# def print_number_of_packages_for_destination():
-#     all_packages = []
-#     op.add_package(all_packages, "10/02/2022", "14/02/2022", "Bucharest",  500)
-#     op.add_package(all_packages, "07/01/2022", "10/01/2022", "Bologna",  250)
-#     op.add_package(all_packages, "01/01/2022", "02/01/2022", "Paris",  1500)
-#     assert op.print_number_of_packages_for_destination(all_packages, "Paris") == 1
+def print_number_of_packages_for_destination():
+    all_packages = []
+    op.add_package(all_packages, 1, "10/02/2022", "14/02/2022", "Bucharest",  500)
+    op.add_package(all_packages, 2, "07/01/2022", "10/01/2022", "Bologna",  250)
+    op.add_package(all_packages, 3, "01/01/2022", "02/01/2022", "Paris",  1500)
+    assert op.print_number_of_packages_for_destination(all_packages, "Paris") == 1
     
     
-# def print_packages_for_duration_and_price():
-#     all_packages = []
-#     op.add_package(all_packages, "10/02/2022", "14/02/2022", "Bucharest", 500)
-#     op.add_package(all_packages, "07/01/2022", "10/01/2022", "Bologna", 250)
-#     op.add_package(all_packages, "01/01/2022", "02/01/2022", "Paris", 1500)
-#     # assert op.print_packages_for_duration_and_price(all_packages, 4, 600) == ["Bucharest\n"]
-#     assert (domain.get_destination(all_packages[0]) == "Bucharest")
-#     assert (domain.get_start_date(all_packages[0]) == "10/02/2022")
-#     assert (domain.get_end_date(all_packages[0]) == "14/02/2022")
-#     assert (domain.get_price(all_packages[0]) == 500)
+def print_packages_for_duration_and_price():
+    all_packages = []
+    op.add_package(all_packages, 1, "10/02/2022", "14/02/2022", "Bucharest", 500)
+    op.add_package(all_packages, 2, "07/01/2022", "10/01/2022", "Bologna", 250)
+    op.add_package(all_packages, 3, "01/01/2022", "02/01/2022", "Paris", 1500)
+    assert (domain.get_destination(all_packages[0]) == "Bucharest")
+    assert (domain.get_start_date(all_packages[0]) == "10/02/2022")
+    assert (domain.get_end_date(all_packages[0]) == "14/02/2022")
+    assert (domain.get_price(all_packages[0]) == 500)
     
     
-# def print_medium_price_for_destination():
-#     all_packages = []
-#     op.add_package(all_packages, "10/02/2022", "14/02/2022", "Bucharest", 500)
-#     op.add_package(all_packages, "07/01/2022", "10/01/2022", "Bucharest", 700)
-#     op.add_package(all_packages,  "01/01/2022", "02/01/2022", "Paris", 1500)
-#     assert op.print_medium_price_for_destination(all_packages, "Bucharest") == 600
+def print_medium_price_for_destination():
+    all_packages = []
+    op.add_package(all_packages, 1, "10/02/2022", "14/02/2022", "Bucharest", 500)
+    op.add_package(all_packages, 2, "07/01/2022", "10/01/2022", "Bucharest", 700)
+    op.add_package(all_packages, 3,  "01/01/2022", "02/01/2022", "Paris", 1500)
+    assert op.print_medium_price_for_destination(all_packages, "Bucharest") == 600
     
 
-# def remove_package_for_destination_higher_price():
-#     all_packages = []
-#     op.add_package(all_packages, "10/02/2022", "14/02/2022", "Bucharest", 500)
-#     op.add_package(all_packages, "07/01/2022", "10/01/2022", "Bucharest", 700)
-#     op.add_package(all_packages, "01/01/2022", "02/01/2022", "Paris", 1500)
-#     op.remove_package_for_destination_higher_price(all_packages, "Bucharest", 600)
-#     assert len(all_packages) == 1
+def remove_package_for_destination_higher_price():
+    all_packages = []
+    op.add_package(all_packages, 1, "10/02/2022", "14/02/2022", "Bucharest", 500)
+    op.add_package(all_packages, 2, "07/01/2022", "10/01/2022", "Bucharest", 700)
+    op.add_package(all_packages, 3, "01/01/2022", "02/01/2022", "Paris", 1500)
+    op.remove_package_for_destination_higher_price(all_packages, "Bucharest", 600)
+    assert len(all_packages) == 1
+    assert (domain.get_id(all_packages[0]) == 1)
+    assert (domain.get_destination(all_packages[0]) == "Bucharest")
+    assert (domain.get_start_date(all_packages[0]) == "10/02/2022")
+    assert (domain.get_end_date(all_packages[0]) == "14/02/2022")
+    assert (domain.get_price(all_packages[0]) == 500)
     
     
-# def remove_package_for_other_month():
-#     all_packages = []
-#     op.add_package(all_packages, "10/10/2022", "14/10/2022", "Bucharest", 500)
-#     op.add_package(all_packages, "01/01/2022", "02/01/2022", "Paris", 1500)
-#     op.remove_package_for_other_month(all_packages, "10")
-#     # assert (domain.get_destination(all_packages[0]) == "Paris")
-#     # assert (domain.get_start_date(all_packages[0]) == "01/01/2022")
-#     # assert (domain.get_end_date(all_packages[0]) == "02/01/2022")
-#     # assert (domain.get_price(all_packages[0]) == 1500)
+def remove_package_for_other_month():
+    all_packages = []
+    op.add_package(all_packages, 1, "01/01/2022", "02/01/2022", "Paris", 1500)
+    op.add_package(all_packages, 2, "10/10/2022", "14/10/2022", "Bucharest", 500)
+    op.remove_package_for_other_month(all_packages, 1)
+    assert (domain.get_destination(all_packages[0]) == "Paris")
+    assert (domain.get_start_date(all_packages[0]) == "01/01/2022")
+    assert (domain.get_end_date(all_packages[0]) == "02/01/2022")
+    assert (domain.get_price(all_packages[0]) == 1500)
     
     
 # def undo_last_operation():
@@ -159,15 +178,13 @@ def all():
     delete_packages_for_price()
     print_packages_for_interval()
     print_packages_for_destination_and_price()
-    # print_packages_for_end_date()
-    # print_number_of_packages_for_destination()
-    # print_packages_for_duration_and_price()
-    # print_medium_price_for_destination()
-    # remove_package_for_destination_higher_price()
-    # remove_package_for_other_month()
+    print_packages_for_end_date()
+    print_number_of_packages_for_destination()
+    print_packages_for_duration_and_price()
+    print_medium_price_for_destination()
+    remove_package_for_destination_higher_price()
+    remove_package_for_other_month()
     # undo_last_operation()
 
 #TODO: resolve bugs for: 
-# print_packages_for_end_date, 
-# remove_package_for_other_month,
 # undo_last_operation
