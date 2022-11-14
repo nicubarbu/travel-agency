b031bad5aaae1ef1ab3b5b9567f306103fb380f8
Nicu Barbu <barbu_nicusor6@yahoo.com>
2022-11-14 22:59:40 +0200

    Update entities.py

--- Domain/entities.py
+++ Domain/entities.py
@@ -1,91 +1,91 @@
-def get_start_date(package):
+def get_start_date(all_packages):
     """
     get the start date of a package
-    input: package - dictionary
+    input: all_packages - dictionary
     return: start_date - string
     """
-    return package["start_date"]
+    return all_packages["start_date"]
 
 
-def get_end_date(package):
+def get_end_date(all_packages):
     """
     get the end date of a package
-    input: package - dictionary
+    input: all_packages - dictionary
     return: end_date - string
     """
-    return package["end_date"]
+    return all_packages["end_date"]
 
 
-def get_destination(package):
+def get_destination(all_packages):
     """
     get the destination of a package
-    input: package - dictionary
+    input: all_packages - dictionary
     return: destination - string
     """
-    return package["destination"]
+    return all_packages["destination"]
 
 
-def get_price(package):
+def get_price(all_packages):
     """
     get the price of a package
-    input: package - dictionary
+    input: all_packages - dictionary
     return: price - float
     """
-    return int(package["price"])
+    return int(all_packages["price"])
 
 
-def get_id(package):
+def get_id(all_packages):
     '''
     get the id of a package
-    input: package - dictionary
+    input: all_packages - dictionary
     return: id - integer
     '''
-    return package["id"]
+    return all_packages["id"]
 
 
-def set_start_date(package, start_date):
+def set_start_date(all_packages, start_date):
     """
     set the start date of a package
-    input: package - dictionary
+    input: all_packages - dictionary
            start_date - string
     """
-    package["start_date"] = start_date
+    all_packages["start_date"] = start_date
     
     
-def set_end_date(package, end_date):
+def set_end_date(all_packages, end_date):
     """
     set the end date of a package
-    input: package - dictionary
+    input: all_packages - dictionary
            end_date - string
     """
-    package["end_date"] = end_date
+    all_packages["end_date"] = end_date
 
 
-def set_destination(package, destination):
+def set_destination(all_packages, destination):
     """
     set the destination of a package
-    input: package - dictionary
+    input: all_packages - dictionary
            destination - string
     """
-    package["destination"] = destination
+    all_packages["destination"] = destination
 
 
-def set_price(package, price):
+def set_price(all_packages, price):
     """
     set the price of a package
-    input: package - dictionary
+    input: all_packages - dictionary
            price - float
     """
-    package["price"] = price
+    all_packages["price"] = price
     
     
-def set_id(package, id):
+def set_id(all_packages, id):
     '''
     set the id of a package
-    input: package - dictionary
+    input: all_packages - dictionary
            id - integer
     '''
-    package["id"] = id
+    all_packages["id"] = id
 
 
 def set_month(month):
